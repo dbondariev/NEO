fa0bdbe69327d261262f877adcf557a3aaca022b
Dima Bondariev <dima.bondariev@gmail.com>
2023-07-07 15:23:57 +0300

    finally figured out how to fix my issue and implemeneted everythign necessery functionality

--- src/components/NeoList.jsx
+++ src/components/NeoList.jsx
@@ -1,9 +1,84 @@
-function NeoList() {
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Box, List, Typography, ListItem } from '@mui/material';
+import NeoCard from './NeoCard';
+
+const NeoList = () => {
+ const [neoData, setNeoData] = useState([]);
+ const [loading, setLoading] = useState(true);
+
+ useEffect(() => {
+  const fetchNeoData = async () => {
+   try {
+    const today = new Date();
+    const startDate = new Date(today.getFullYear(), today.getMonth(), 1);
+    const formattedStartDate = startDate.toISOString().split('T')[0];
+
+    const response = await axios.get(
+     `https://api.nasa.gov/neo/rest/v1/feed?start_date=${formattedStartDate}&end_date=${
+      today.toISOString().split('T')[0]
+     }&api_key=${import.meta.env.VITE_REACT_APP_API_KEY}
+`,
+    );
+
+    const neoData = response.data.near_earth_objects;
+
+    const neoCards = Object.keys(neoData).map(date => {
+     const neoDate = neoData[date];
+     const maxDiameter = Math.max(
+      ...neoDate.map(neo => neo.estimated_diameter.kilometers.estimated_diameter_max),
+     );
+     const hazardousCount = neoDate.filter(neo => neo.is_potentially_hazardous_asteroid).length;
+     const closestNeo = Math.min(
+      ...neoDate.map(neo => neo.close_approach_data[0].miss_distance.kilometers),
+     );
+     const fastestNeo = Math.max(
+      ...neoDate.map(neo => neo.close_approach_data[0].relative_velocity.kilometers_per_hour),
+     );
+
+     return {
+      date,
+      maxDiameter,
+      hazardousCount,
+      closestNeo,
+      fastestNeo,
+     };
+    });
+
+    setNeoData(neoCards);
+    setLoading(false);
+   } catch (error) {
+    console.error('Error fetching NEO data:', error);
+    setLoading(false);
+   }
+  };
+
+  fetchNeoData();
+ }, []);
+
  return (
-  <div>
-   <h1>Neo List</h1>
-  </div>
+  <Box>
+   <Typography variant="h4">NEO List</Typography>
+   {loading ? (
+    <Typography>Loading data...</Typography>
+   ) : (
+    <List>
+     {neoData.map(neo => (
+      <ListItem key={neo.date}>
+       <NeoCard
+        date={neo.date}
+        maxDiameter={neo.maxDiameter}
+        hazardousCount={neo.hazardousCount}
+        closestNeo={neo.closestNeo}
+        fastestNeo={neo.fastestNeo}
+        isTopHazardous={neo.hazardousCount >= 2}
+       />
+      </ListItem>
+     ))}
+    </List>
+   )}
+  </Box>
  );
-}
+};
 
 export default NeoList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
 import { CssBaseline } from '@mui/material';
 
-ReactDOM.render(
- <React.StrictMode>
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+ <>
   <CssBaseline />
   <App />
- </React.StrictMode>,
- document.getElementById('root'),
+ </>,
 );
