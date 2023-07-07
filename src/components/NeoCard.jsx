ff898c04449d3d8e116ba487e4e2d73e56323074
Dima Bondariev <dima.bondariev@gmail.com>
2023-07-07 11:10:29 +0300

    created omponents, NeoCard, NeoList and fetch api file

---
+++ src/components/NeoCard.jsx
@@ -0,0 +1,9 @@
+function NeoCard() {
+ return (
+  <div>
+   <h1>Neo Card</h1>
+  </div>
+ );
+}
+
+export default NeoCard;
---
+++ src/components/NeoList.jsx
@@ -0,0 +1,9 @@
+function NeoList() {
+ return (
+  <div>
+   <h1>Neo List</h1>
+  </div>
+ );
+}
+
+export default NeoList;
