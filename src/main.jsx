84e0db7c2b730f3286bd8c6fbd6047002b96eeb4
Dima Bondariev <dima.bondariev@gmail.com>
2023-07-07 11:02:34 +0300

    installed Material UI and imported cssBaseLine to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { CssBaseline } from '@mui/material';
+
+ReactDOM.render(
+ <React.StrictMode>
+  <CssBaseline />
+  <App />
+ </React.StrictMode>,
+ document.getElementById('root'),
+);
